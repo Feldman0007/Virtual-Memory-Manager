a93f7a60a90179f0fd4416854d56edfff23e69c5
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2019-04-19 14:38:49 -0700

    Update TLB.h

--- TLB.h
+++ TLB.h
@@ -3,11 +3,22 @@
 #define TLB_H
 #define TLB_SIZE 16
 
+#include "ReplacementAlgorithm.h"
+
+/*
+class TLBReplacementAlgorithm : public ReplacementAlgorithm {
+};
+*/
 class TLB {
 	private:
-		int tlb[TLB_SIZE];
-	public:
+		//ReplacementAlgorithm replacementAlgorithm;
 
+		//Array of tlb page table
+		//int translationLookasideBuffer[TLB_SIZE];
+
+	public:
+		//int tlbLookup;
+		//bool isFull;
 };
 
 #endif

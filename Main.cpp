d64547a2b6936a7d7d3f5a645b2b9f3db42fd850
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2019-04-19 14:35:49 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,76 +1,70 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "MMU.h"
+#include "VMM.h"
 
 using namespace std;
 
 int main() {
 
-    //filename
-    string filename = "addresses.txt";
+	//filename
+	string filename = "Addresses.txt";
 
 	ifstream fileReader;
 	fileReader.open(filename);
 
 	//Each time we read we call mmu to convert logical to physical 
-	MMU mmu;	
-	string tempAddress; //remove later
+	
+	
+	VMM virtualMemoryManager; // Virtually memory manager is our over arching class that encompasses all domain objects of a virtual memory manager
+	string input; //remove later
 
 	//logicalAddress.setAddress(address);
-	
-	while (fileReader >> tempAddress) {
-		mmu.process(tempAddress);
-		//look in page table
-		//physical address = (calculate physical address)
-		//use that address to index into RAM
-		//retreive value from rAM
-		//store to physicalAddress to std out
+
+	while (fileReader >> input) {
+		virtualMemoryManager.processInput(input);	
+		//Process is the primary function of VMM input is will:
+		// 1) Extract page number p so we may use it to index into the page table
+		// 2) Use the page number p to index into the page table to retreive frame number f 
+		// 3) Replace the page number p in the logical address with the frame number f.
+		// 4) Use that physical address to locate the and output the value of the byte stored in RAM
+		// 5) Convert to hex to print 
+	}
+
+	fileReader.close();
+	system("PAUSE");
+	return 0;
+	/*
+		std::ifstream infile("address.txt");
+		int a;
+		while (infile >> a)
+		{
+		// process a
+		}
+
+		backing_store = fopen("BACKING_STORE.bin");
+		if (backing_store = NULL) {
+			cout << "Error opening backing_store.bin" << endl;
+			return -1;
+		}
+
+		int numOfphysicalAddress = 0;
+		hexConvertion(physicalAddress);
 
 
+		double pfRate = pageFaults / (double)numOfphysicalAddress;
+		double TLBRate = TLBHits / (double)numOfphysicalAddress;
+	*/
+}
 
 /*
-std::ifstream infile("address.txt");
-int a;
-while (infile >> a)
-{
-    // process a
-}
-
-*/
-
-	}
-
-    //pass filename to MMU
-	MMU mmu;
-    mmu.process();
-
-
-	backing_store = fopen("BACKING_STORE.bin");
-	if (backing_store = NULL) {
-		cout << "Error opening backing_store.bin" << endl;
-		return -1;
-	}
-
-	int numOfphysicalAddress = 0; 
-	hexConvertion(physicalAddress);
-
-
-	/*
-	double pfRate = pageFaults / (double)numOfphysicalAddress;
-	double TLBRate = TLBHits / (double)numOfphysicalAddress;
-	*/
-
-	return 0;
-}
-
 
 void hexConvertion(int address)
 {
 	char hexNum[100];
 	int i = 0;
 	while (n != 0)
-	{ 
+	{
 		int temp = 0;
 		temp = n % 16;
 		if (temp < 10)
@@ -90,15 +84,15 @@ void hexConvertion(int address)
 }
 
 
-
+*/
 
 /*
 Use at TLB
-    1. FIFO
-        2nd chance/enhanced 2nd chance
-    2. LRU
-    3. MFU
-    4. LFU
+	1. FIFO
+		2nd chance/enhanced 2nd chance
+	2. LRU
+	3. MFU
+	4. LFU
 
 
 */
